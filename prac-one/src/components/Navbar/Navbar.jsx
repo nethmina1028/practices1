d2c55a591a66fcfd8b5d4fbfd101766b27d8d07e
Wickramasinghe W A N P <139307869+nethmina1028@users.noreply.github.com>
2024-06-24 19:16:25 +0530

    rounded nev sm

--- prac-one/src/components/Navbar/Navbar.jsx
+++ prac-one/src/components/Navbar/Navbar.jsx
@@ -11,23 +11,23 @@ function Navbar() {
       ];
       
   return (
-   <nav className='w-full bg-gradient-to-r from-secondary to-secondary/90 py-2 ' >
-    <div className='container max-container padding-container top-0  ' >
-    <div className='flex justify-between flex-row '>
-        <a href="#" className='flex items-center font-cursive font-bold text-2xl  gap-2 text-white tracking-wider ' >
+   <nav className='w-full py-2 bg-gradient-to-r from-secondary to-secondary/90 ' >
+    <div className='container top-0 max-container padding-container ' >
+    <div className='flex flex-row justify-between '>
+        <a href="#" className='flex items-center gap-2 text-2xl font-bold tracking-wider text-white font-cursive ' >
            <img src={Logo} alt="logo" width={64} height={29} />
            Coffee Cafe
         </a>
        
-       <div className='lg:flex items-center gap-8 hidden text-neutral-400 '>
-       <ul className='items-center gap-4 text-xl font-sm space-x-8  hidden lg:flex ml-14 p-2  '>
+       <div className='items-center hidden gap-8 lg:flex text-neutral-400 '>
+       <ul className='items-center hidden gap-4 p-2 space-x-8 text-xl font-sm lg:flex ml-14 '>
         {navItems.map((item,index) =>(
                        <li key={index}>
                         <a href={item.href} className='hover:text-white '>{item.label}</a>
                        </li> 
                     ))}
        </ul>
-       <button className='text-md px-8 py-2 border rounded-full bg-primary hover:text-white hover:bg-brandDark ' >Order
+       <button className='px-8 py-2 border rounded-sm text-md bg-primary hover:text-white hover:bg-brandDark ' >Order
          </button>
 
        </div>
